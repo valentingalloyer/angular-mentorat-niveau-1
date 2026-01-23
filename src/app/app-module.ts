83f36652e5ed6b7a897081510c690e2561f92525
Valentin <valentin.galloyer@gmail.com>
2026-01-23 19:39:29 +0100

    module

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Header } from './components/header/header';
 import { Ex1 } from './components/ex1/ex1';
+import {TaskItem} from './components/task-item/task-item';
 
 @NgModule({
   declarations: [
     App,
     Header,
-    Ex1
+    Ex1,
+    TaskItem
   ],
   imports: [
     BrowserModule,
