c5f71b9b59ae1025db7e410028a4550b4da58393
Valentin <valentin.galloyer@gmail.com>
2026-01-23 19:21:48 +0100

    ex2

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Header } from './components/header/header';
 import { Ex1 } from './components/ex1/ex1';
+import { Ex2 } from './components/ex2/ex2';
 
 @NgModule({
   declarations: [
     App,
     Header,
-    Ex1
+    Ex1,
+    Ex2
   ],
   imports: [
     BrowserModule,
