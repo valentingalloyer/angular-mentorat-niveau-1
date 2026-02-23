96722e1adad66807f73b6a9c8f897f559aa1d3ce
Valentin <valentin.galloyer@gmail.com>
2026-02-23 16:29:56 +0100

    load mock amélioré

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,30 +14,32 @@ export class App implements OnInit {
   showList = true;
 
   // Injection de dépendance : Angular fournit TaskService automatiquement
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService) {
+  }
 
   ngOnInit() {
-    this.tasks = this.taskService.getTasks();
+    // this.tasks = this.taskService.getTasks();
+    this.loadFromApi();
   }
 
   addTask(task: string): void {
     this.taskService.addTask(task);
-    this.tasks = this.taskService.getTasks();
+    // this.tasks = this.taskService.getTasks();
   }
 
   removeTask(index: number): void {
     this.taskService.removeTask(index);
-    this.tasks = this.taskService.getTasks();
+    // this.tasks = this.taskService.getTasks();
   }
 
-  loadFromApi(): void {
-    this.taskService.loadMockTodosFromApi(5).subscribe({
-      next: (apiTasks) => {
-        this.tasks = apiTasks;
-      },
-      error: () => {
-        console.log('Erreur lors du chargement API');
-      },
+  loadFromApi(limit = 5): void {
+    this.taskService.loadMockTodosFromApi().subscribe({
+      next: (apiTasks: any[]) => {
+        this.tasks = apiTasks.slice(0, limit).map((t: any) => t.title);
+        },
+      error: (err) => {
+        console.log('Erreur lors du chargement API', err);
+      }
     });
   }
 }
